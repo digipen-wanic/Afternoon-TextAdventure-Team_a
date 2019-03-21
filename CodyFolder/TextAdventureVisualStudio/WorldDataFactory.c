b3edb3442d382c576d87c9632cf79e7035e38ac4
CodySpring33 <48695920+CodySpring33@users.noreply.github.com>
2019-03-21 15:07:11 -0700

    my version

--- CodyFolder/TextAdventureVisualStudio/WorldDataFactory.c
+++ CodyFolder/TextAdventureVisualStudio/WorldDataFactory.c
@@ -15,11 +15,13 @@ This could be used to create default states as well as loaded state.
 #include "WorldData.h" /* WorldData_Create, WorldData_SetRoom */
 #include "Room.h" /* Room_Create, Room_AddRoomExit, Room_GetItemList */
 #include "ItemList.h" /* ItemList_Add */
+#include "GameState.h"
 #include "BrickFunctions.h" /* Brick_Build */
 #include "GoldPieceFunctions.h" /* GoldPiece_Build */
 #include "ExitDoorFunctions.h" /* ExitDoor_Build */
-#include "FlashLightFunctions.h" //Fuse_Build
-
+#include "MEDKITFunctions.h" /* MEDKIT_Build */
+#include "FlashLightFunctions.h" /*Flashlight_Build*/
+#include "FuseFunctions.h" 
 
 
 /******************************************************************************
@@ -61,8 +63,8 @@ Room* Room1_Build_StartArea()
 	Room_AddRoomExit(room, "door", 3);  /* 1 = the room index this exit connects to */
 										 /* Items
 										 add items to the room */
+	ItemList_AddItem(Room_GetItemList(room), Fuse_Build());
 	ItemList_AddItem(Room_GetItemList(room), Flashlight_Build());
-
 	/* Return the new room */
 	return room;
 }
@@ -93,37 +95,39 @@ Room* Room3_BuildMainHallway()
 	/* Pre-declare a room pointer which we will use to build the new room */
 	Room* room;
 
+	/* if you are injured when you entered the room you lose some of your health */
+
+
 	/* Create the room
 	include an initial room description */
-	room = Room_Create("DEBUG: This is a template - Include a description for the room here\n");
+	room = Room_Create("Standing at the end of the hallway you see the room you started in on your right. \nFarther on your right you see the Workroom \nOn your left you see the Capton's room \nFarther on your right you see the elevator room \nAt the end of the hall you see the air lock \n");
 
 	/* Exits
 	add one or more exits to allow navigation between rooms */
-	Room_AddRoomExit(room, "north", 1);  /* 1 = the room index this exit connects to */
+	Room_AddRoomExit(room, "starting room", 1);  /* 1 = the room index this exit connects to */
 
 										 /* Items
 										 add items to the room */
-	ItemList_AddItem(Room_GetItemList(room), ExitDoor_Build());
 
 	/* Return the new room */
 	return room;
 }
-Room* Room4_BuildWorkrooom()
+Room* Room4_BuildMedbay()
 {
 	/* Pre-declare a room pointer which we will use to build the new room */
 	Room* room;
 
 	/* Create the room
 	include an initial room description */
-	room = Room_Create("DEBUG: This is a template - Include a description for the room here\n");
+	room = Room_Create("The medbay has paper scattered about. Documentation from everyone who was on board and the intended plans of colonization. \nThere is a medkit on the ground\n");
 
 	/* Exits
 	add one or more exits to allow navigation between rooms */
-	Room_AddRoomExit(room, "north", 1);  /* 1 = the room index this exit connects to */
-
+	Room_AddRoomExit(room, "door", 3);  /* 1 = the room index this exit connects to */
+	Room_AddRoomExit(room, "hallway", 3);
 										 /* Items
 										 add items to the room */
-	ItemList_AddItem(Room_GetItemList(room), ExitDoor_Build());
+	ItemList_AddItem(Room_GetItemList(room), MEDKIT_Build());
 
 	/* Return the new room */
 	return room;
@@ -159,12 +163,12 @@ Room* Room6_Build_AirLock()
 	/* TODO REQUIRED: Call Room_Create with the Room 1 description:
 	"This is room 6. It is a display room with a cage in the middle. You can see a jeweled egg inside the cage.  There is a crack in the west wall, but you can't fit through it from this side.\n" */
 
-	room = Room_Create("DEBUG: this is a template - Include a description for the room here\n");
+	room = Room_Create("as you enter the airlock you notice the lever is still in a functing state and there is a handle next to it that is also undamaged.\n you see the doors/exits are on the north and south\n");
 
 	/* TODO REQUIRED: Add an Exit "north" to Room 3 and 7 */
 
 	Room_AddRoomExit(room, "north", 3);
-	Room_AddRoomExit(room, "South", 7);
+	Room_AddRoomExit(room, "south", 7);
 
 	/* TODO BASIC: Add room exit shortcut for "n" */
 
@@ -183,15 +187,14 @@ Room* Room7_Build_Lounge()
 
 	/* Create the room
 	include an initial room description */
-	room = Room_Create("DEBUG: This is a template - Include a description for the room here\n");
+	room = Room_Create("A fairly large lounge with a set of tables, chairs, and TVs.\nReally just a standard relaxation area. There are three doors in the room, two go to the engine bays and one to the air lock.\n");
 
 	/* Exits
 	add one or more exits to allow navigation between rooms */
-	Room_AddRoomExit(room, "north", 1);  /* 1 = the room index this exit connects to */
-
-										 /* Items
+	Room_AddRoomExit(room, "air lock", 6);  /* 1 = the room index this exit connects to */
+	Room_AddRoomExit(room, "engine room right", 9);
+	Room_AddRoomExit(room, "engine room left", 10);										 /* Items
 										 add items to the room */
-	ItemList_AddItem(Room_GetItemList(room), ExitDoor_Build());
 
 	/* Return the new room */
 	return room;
@@ -227,16 +230,13 @@ Room* Room9_BuildEngineRoom1()
 	/* TODO REQUIRED: Call Room_Create with the Room 1 description:
 	"This is room 1.  There is a large mirror here, and it shimmers as you approach.\n" */
 
-	room = Room_Create("DEBUG: this is a template - include a description for this room");
+	room = Room_Create("As you enter the right engine room you notice the engine has a broken quantum carborator and the tools to fix it are not in the room\n you will need to go to the forwad bay and pickup the replacement quantum carborator and your tools are in the other engine room\n fixing the engine is of the upmost importance\n");
 
 	/* TODO REQUIRED: Add an Exit to Room 7 */
 
 	Room_AddRoomExit(room, "east", 7);
 
-	/* TODO BASIC: Add exit shortcuts for "through mirror" and "mirror" */
-
-	/* TODO REQUIRED: Add an Exit "south" back to Room 0 */
-	/* TODO BASIC: Add room exit shortcut for "s" */
+	Room_AddRoomExitShortcut(room, "e", 7);
 
 	/* TODO REQUIRED: Add a brick to the list of items in the room */
 
@@ -254,13 +254,13 @@ Room* Room10_BuildEngineRoom2()
 	/* TODO REQUIRED: Call Room_Create with the Room 2 description:
 	"This is room 2.  The room is isolated from the others, but you can see a crack in the east wall, just large enough to get through.\n" */
 
-	room = Room_Create("DEBUG: this is a template - include a description for this room");
+	room = Room_Create("as you enter the right engine you see that the engine is in working order and only has to be turned back on\n you also notice that your tools are in the closet along the far wall\n you will need these to fix the left engine");
 
 	/* TODO REQUIRED: Add an Exit "east" to Room 7 */
 
 	Room_AddRoomExit(room, "west", 7);
 
-	/* TODO BASIC: Add exit shortcuts for "e" and "crack" */
+	Room_AddRoomExitShortcut(room, "w", 7);
 
 	/* TODO REQUIRED: Add a gold piece to the list of items in the room */
 
@@ -304,23 +304,23 @@ WorldData* CreateInitialWorldData()
 
 	/* TODO REQUIRED: update room count to match the number of rooms you have created and added to the world
 	   if this number doesn't match then your game will either crash or you will end up stuck in a broken room with no exits */
-	int roomCount = 1;
+	int roomCount = 12;
 
 	/* create the new WorldData object with 3 rooms */
 	worldData = WorldData_Create("Welcome to my GAM100 Game!\n\n", roomCount);
 
 	/* TODO REQUIRED: add rooms to the world data */
-	WorldData_SetRoom(worldData, 1, Room1_Build_StartArea);
-	WorldData_SetRoom(worldData, 2, Room2_Build_ForwardBay);
-	WorldData_SetRoom(worldData, 3,	Room3_BuildMainHallway);
-	WorldData_SetRoom(worldData, 4, Room4_BuildWorkrooom);
-	WorldData_SetRoom(worldData, 5, Room5_Build_ElevatorRoom);
-	WorldData_SetRoom(worldData, 6, Room6_Build_AirLock);
-	WorldData_SetRoom(worldData, 7, Room7_Build_Lounge);
-	WorldData_SetRoom(worldData, 8, Room8_Build_CaptainsRoom);
-	WorldData_SetRoom(worldData, 9, Room9_BuildEngineRoom1);
-	WorldData_SetRoom(worldData, 10, Room10_BuildEngineRoom2);
-	WorldData_SetRoom(worldData, 11, Room11_BuildTheBridge);
+	WorldData_SetRoom(worldData, 1, Room1_Build_StartArea());
+	WorldData_SetRoom(worldData, 2, Room2_Build_ForwardBay());
+	WorldData_SetRoom(worldData, 3,	Room3_BuildMainHallway());
+	WorldData_SetRoom(worldData, 4, Room4_BuildMedbay());
+	WorldData_SetRoom(worldData, 5, Room5_Build_ElevatorRoom());
+	WorldData_SetRoom(worldData, 6, Room6_Build_AirLock());
+	WorldData_SetRoom(worldData, 7, Room7_Build_Lounge());
+	WorldData_SetRoom(worldData, 8, Room8_Build_CaptainsRoom());
+	WorldData_SetRoom(worldData, 9, Room9_BuildEngineRoom1());
+	WorldData_SetRoom(worldData, 10, Room10_BuildEngineRoom2());
+	WorldData_SetRoom(worldData, 11, Room11_BuildTheBridge());
 
 	/* TODO ADVANCED: add additional advanced rooms */
 
