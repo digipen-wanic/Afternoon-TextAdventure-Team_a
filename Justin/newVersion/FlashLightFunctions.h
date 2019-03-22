5c179a7d998367ef2dbb133c9dff95fb35acbced
CodySpring33 <48695920+CodySpring33@users.noreply.github.com>
2019-03-21 19:51:36 -0700

    added new versions to each of the personal folders

---
+++ Justin/newVersion/FlashLightFunctions.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+filename    FlashlightFunctions.h
+author      Cody Spring
+course      GAM100 ** Do not use this code in your team project
+
+Brief Description:
+This file declares the functions to create a specific item, the "Flashlight".
+
+******************************************************************************/
+#pragma once
+
+typedef struct Item Item;
+
+
+/* Build a "Flashlight" object */
+Item* Flashlight_Build();
